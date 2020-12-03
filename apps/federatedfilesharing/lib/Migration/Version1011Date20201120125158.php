30d04b75fdf2bd36858dd0ee5c9fb5bbcc8240ac
Julius Härtl <jus@bitgrid.net>
2020-12-03 17:39:56 +0100

    Adjust default
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- apps/federatedfilesharing/lib/Migration/Version1011Date20201120125158.php
+++ apps/federatedfilesharing/lib/Migration/Version1011Date20201120125158.php
@@ -29,23 +29,42 @@ use Closure;
 use Doctrine\DBAL\Types\Type;
 use Doctrine\DBAL\Types\Types;
 use OCP\DB\ISchemaWrapper;
+use OCP\IDBConnection;
 use OCP\Migration\IOutput;
 use OCP\Migration\SimpleMigrationStep;
 
 class Version1011Date20201120125158 extends SimpleMigrationStep {
+
+	/** @var IDBConnection */
+	private $connection;
+
+	public function __construct(IDBConnection $connection) {
+		$this->connection = $connection;
+	}
+
 	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options) {
 		/** @var ISchemaWrapper $schema */
 		$schema = $schemaClosure();
-		
+
 		if ($schema->hasTable('federated_reshares')) {
 			$table = $schema->getTable('federated_reshares');
 			$remoteIdColumn = $table->getColumn('remote_id');
 			if ($remoteIdColumn && $remoteIdColumn->getType()->getName() !== Types::STRING) {
+				$remoteIdColumn->setNotnull(false);
 				$remoteIdColumn->setType(Type::getType(Types::STRING));
 				$remoteIdColumn->setOptions(['length' => 255]);
+				$remoteIdColumn->setDefault('');
 			}
 		}
 
 		return $schema;
+	}
+
+	public function postSchemaChange(IOutput $output, \Closure $schemaClosure, array $options) {
+		$qb = $this->connection->getQueryBuilder();
+		$qb->update('federated_reshares')
+			->set('remote_id', $qb->createNamedParameter(''))
+			->where($qb->expr()->eq('remote_id', $qb->createNamedParameter('-1')));
+		$qb->execute();
 	}
 }
